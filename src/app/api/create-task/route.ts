be8c45dab4f4e2593c6db5cecc49f845525f5bf3
sonaodupara <sonaoduparaa@gmail.com>
2026-05-27 13:11:25 +0530

    Fix language code to en

--- src/app/api/create-task/route.ts
+++ src/app/api/create-task/route.ts
@@ -19,7 +19,7 @@ async function sendWhatsAppMessage(to: string, shortId: string, taskTitle: strin
         type: "template",
         template: {
           name: "task_assigned",
-          language: { code: "en_GB" },
+          language: { code: "en" },
           components: [
             {
               type: "body",
